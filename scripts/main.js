2418cd7c0baf23adf0c7f73d886a128989d30141
stepanhampl <koblihh1@gmail.com>
2021-12-14 23:57:11 +0100

    checkpoint

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,25 @@
-const navbarLinks = document.getElementsByClassName("navbar-links")[0]
-const hamburger = document.getElementsByClassName("hamburger")[0]
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const hamburger = document.getElementsByClassName("hamburger")[0];
+const nonNavbar = document.getElementById("non-navbar");
 
 
 
 hamburger.addEventListener("click", () => {
     if (navbarLinks.style.display != "flex") {
         navbarLinks.style.display = "flex";
+        nonNavbar.style.top = "7.5rem";
+        console.log("It was executed!")
     } else {
         navbarLinks.style.display = "";
+        nonNavbar.style.top = "0";
     }
-})
+});
 
-// hamburger.addEventListener("click", () => {
-//     navbarLinks.classList.toggle("visible")
-// })
+window.addEventListener("resize", function (event) {
+    if (window.matchMedia("(min-width: 700px)").matches) {
+        navbarLinks.style.display = "";
+        nonNavbar.style.top = "0";
+    }
+});
+
+// nonNavbar.style.top = "50rem"
