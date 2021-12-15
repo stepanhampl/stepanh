f693970b1a331dcdf6f48f7c68a5dd152c0ad605
stepanhampl <koblihh1@gmail.com>
2021-12-15 09:04:15 +0100

    backup

--- scripts/main.js
+++ scripts/main.js
@@ -3,18 +3,18 @@ const hamburger = document.getElementsByClassName("hamburger")[0];
 const nonNavbar = document.getElementById("non-navbar");
 
 
-
+// BELOW: show/hide menu on click on .hamburger (which is active while screen is small (now: max-width=700px))
 hamburger.addEventListener("click", () => {
     if (navbarLinks.style.display != "flex") {
         navbarLinks.style.display = "flex";
         nonNavbar.style.top = "7.5rem";
-        console.log("It was executed!")
     } else {
         navbarLinks.style.display = "";
         nonNavbar.style.top = "0";
     }
 });
 
+// BELOW: hide potentially shown menu while screen is getting bigger than 700px
 window.addEventListener("resize", function (event) {
     if (window.matchMedia("(min-width: 700px)").matches) {
         navbarLinks.style.display = "";
@@ -22,4 +22,4 @@ window.addEventListener("resize", function (event) {
     }
 });
 
-// nonNavbar.style.top = "50rem"
+//BELOW: on click on .navbar-logo, show up emoji (maybe random in future), which is in .animation-01 on random fixed place on page
