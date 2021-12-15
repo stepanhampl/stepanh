9aad5581bcfae3c2559f2858d10de072bbee0db5
stepanhampl <koblihh1@gmail.com>
2021-12-15 15:10:03 +0100

    backup before finishing random emojis

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ const animation01 = document.getElementsByClassName("animation-01")[0];
 const navbarLogo = document.getElementsByClassName("navbar-logo")[0];
 
 
-// BELOW: show/hide menu on click on .hamburger (which is active while screen is small (now: max-width=700px))
+// show/hide menu on click on .hamburger (which is active while screen is small (now: max-width=700px))
 hamburger.addEventListener("click", () => {
     if (navbarLinks.style.display != "flex") {
         navbarLinks.style.display = "flex";
@@ -16,7 +16,7 @@ hamburger.addEventListener("click", () => {
     }
 });
 
-// BELOW: hide potentially shown menu while screen is getting bigger than 700px
+// hide potentially shown menu while screen is getting bigger than 700px
 window.addEventListener("resize", function (event) {
     if (window.matchMedia("(min-width: 700px)").matches) {
         navbarLinks.style.display = "";
@@ -24,13 +24,21 @@ window.addEventListener("resize", function (event) {
     }
 });
 
-//BELOW: on click on .navbar-logo, show up emoji (maybe random in future), which is in .animation-01 on random fixed place on page
+// on click on .navbar-logo, show up emoji (maybe random in future), which is in .animation-01 on random fixed place on page
 navbarLogo.addEventListener("click", () => {
     let randLeft = Math.floor(Math.random() * 101);
     let randTop = Math.floor(Math.random() * 101);
-    animation01.style.display = "initial";
-    animation01.style.left = randLeft + "%";
-    animation01.style.top = randTop + "%";
+
+    // loop ensuring, that real entity code has been displayed
+    // while (animation01.offsetHeight == "auto") {}
+    // come up with random number. 127344 ~ minimal HTML entity, 129510 ~ maximal HTML entity.
+    do {
+        let emojiNum = Math.floor(Math.random() * (129510 - 127344 + 1) + 127344);
+        animation01.innerHTML = `&#${emojiNum};`;
+        animation01.style.display = "initial";
+        animation01.style.left = randLeft + "%";
+        animation01.style.top = randTop + "%";
+    } while (animation01.offsetHeight > 0);
 
     // make the emoji disappear
     let delay = 200;
